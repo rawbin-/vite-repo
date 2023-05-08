77e0b08d1ce40c441d1cf2be2edb19d3a946eb94
rawbin.zhang <rawbin.zhang@lunarax.io>
2023-05-08 19:54:48 +0800

    init demo

---
+++ src/views/Grafana.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <iframe src="/grafana" width=80% height=80%></iframe>
+  </main>
+</template>
